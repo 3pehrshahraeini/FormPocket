9caacbd8c45eddff38c7cc1cd88cfbfcab300273
shahraeinisepehr <shahraeinisepehr@gmail.com>
2020-11-21 20:23:57 +0330

    update4

--- FormPocket/app/controllers/SecurityController.js
+++ FormPocket/app/controllers/SecurityController.js
@@ -1,4 +1,38 @@
 ﻿'use strict';
 app.controller('SecurityController', ['$scope', '$location', '$routeParams', '$rootScope', '$route', function ($scope, $location, $routeParams, $rootScope, $route) {
 
+
+    $scope.btn_new = function () {
+        $scope.popup_new_visible = true;
+    }
+
+    $scope.popup_new = {
+        shading: true,
+        fullScreen: true,
+        showTitle: true,
+        dragEnabled: true,
+
+        visible: false,
+
+        closeOnOutsideClick: false,
+        onTitleRendered: function (e) {
+
+        },
+        onShowing: function (e) {
+
+        },
+        onShown: function (e) {
+
+
+        },
+        onHiding: function () {
+
+
+            $scope.popup_new_visible = false;
+
+        },
+        bindingOptions: {
+            visible: 'popup_new_visible',
+        }
+    };
 }]);
